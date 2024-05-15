63f3812f8f50f8d8033b2c0a3d19d9abc87bc1a9
Emanuel Valente <emavalente.dev@gmail.com>
2024-05-15 19:27:54 -0300

    reparando pagina event-list

--- js/event-list.js
+++ js/event-list.js
@@ -1,5 +1,6 @@
 const filterOptions = document.querySelectorAll(".filter-item");
 const eventList = document.querySelector("#event-list");
+const filterSelect = document.querySelector(".select-categories");
 
 filterOptions.forEach((filter) => {
   filter.addEventListener(
@@ -7,3 +8,8 @@ filterOptions.forEach((filter) => {
     () => (eventList.textContent = `Mostrando listado de ${filter.textContent}`)
   );
 });
+
+filterSelect.addEventListener(
+  "change",
+  (e) => (eventList.textContent = `Mostrando listado de ${e.target.value}`)
+);
